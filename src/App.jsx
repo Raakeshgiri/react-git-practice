74c22a293d16934f97486f55aeaaee07e7c23847
Raakeshgiri <raakesh910@gmail.com>
2025-07-09 14:55:50 +0530

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './component/Navbar'
 
 const App = () => {
   return (
     <>
+        <Navbar/>
         <h1>APP Component</h1>
     </>
   )
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <h1 className="logo">logo</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </>
+  )
+}
+
+export default Navbar
